43f4ea9b3bdef3d9352902558ab0bc634a69afe2
vpetrakis <123031812+vpetrakis@users.noreply.github.com>
2026-04-21 16:26:27 +0300

    Update __init__.py

--- spatial_matrix/__init__.py
+++ spatial_matrix/__init__.py
@@ -1,1 +1,11 @@
+import os
+import streamlit.components.v1 as components
 
+# This physically locates the compiled React code folder
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+build_dir = os.path.join(parent_dir, "frontend/build")
+_spatial_matrix = components.declare_component("spatial_matrix", path=build_dir)
+
+def spatial_risk_matrix(data_json, key=None):
+    """Sends JSON to React, returns user interaction back to Python."""
+    return _spatial_matrix(data=data_json, key=key, default=None)
